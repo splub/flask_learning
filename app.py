b63e577fc8433345c0e17053d7bc63d8d87de2ad
as <as@ass-MacBook-Pro.local>
2017-06-01 18:55:39 +0800

    url规则中结尾带斜杠和不带斜杠是两个url

--- app.py
+++ app.py
@@ -8,6 +8,14 @@ def index():
 def user_id(num_for):
     return 'hello %d' % num_for
 
+@app.route('/about')
+def about():
+    return ('learning flask')
+
+@app.route('/about/')
+def path():
+    return 'this is a path'
+
 if __name__ == '__main__':
 
     app.run(host='0.0.0.0', debug=True)

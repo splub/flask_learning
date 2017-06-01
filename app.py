0cc539e5c9cb145f22853a673c9c429bdcff05a7
as <as@ass-MacBook-Pro.local>
2017-06-01 18:49:43 +0800

    添加变量，变量支持int,float,string,path

--- app.py
+++ app.py
@@ -2,9 +2,12 @@ from flask import Flask
 app=Flask(__name__)
 
 @app.route('/')
-def hello_word():
-    return ('Hello World!')
+def index():
+    return ("Index Page")
+@app.route('/user/<int:num_for>')
+def user_id(num_for):
+    return 'hello %d' % num_for
 
 if __name__ == '__main__':
-    
+
     app.run(host='0.0.0.0', debug=True)
